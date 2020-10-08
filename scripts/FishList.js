c781f7c55bccbc582497e2d55179f09a45efd8d7
Heath Lester <h.allen.lester@gmail.com>
2020-10-08 14:39:44 -0500

    Fish Successfully Rendered but CSS Flex is broke

--- scripts/FishList.js
+++ scripts/FishList.js
@@ -1,14 +1,22 @@
 
 import {  useFish  } from './FishDataProvider.js';
+import {  Fish  } from './Fish.js';
+
 
 export const FishList = () => {
-    // grabs the HTML element and 
+    // grabs the HTML element and stores it in fishes variable
     const contentElement = document.querySelector(".fishList")
     const fishes = useFish()
 
+    let fishHTMLRepresentations = ""
+    for (const fish of fishes) {
+      fishHTMLRepresentations += Fish(fish)
+        
+    }
+
     contentElement.innerHTML += `
         <div class="fishList">
-            All the fish go here!
+            ${fishHTMLRepresentations}
         </div>
         `
 }
