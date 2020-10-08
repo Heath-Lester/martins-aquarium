8afc11b1a3003ca7b66d283f9a7350e9cbe3d05e
Heath Lester <h.allen.lester@gmail.com>
2020-10-08 14:13:51 -0500

    Added HTML Targeter module as FishList.js

--- scripts/FishList.js
+++ scripts/FishList.js
@@ -1,2 +1,14 @@
 
+import {  useFish  } from './FishDataProvider.js';
 
+export const FishList = () => {
+    // grabs the HTML element and 
+    const contentElement = document.querySelector(".fishList")
+    const fishes = useFish()
+
+    contentElement.innerHTML += `
+        <div class="fishList">
+            All the fish go here!
+        </div>
+        `
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,12 @@
 // This logs all the properties of all the objects from fishCollection
 
 import {  useFish  } from './FishDataProvider.js';
+import {  FishList  } from './FishList.js';
 
 const allTheFish = useFish()
 
 for (const fish of allTheFish) {
     console.log(fish)
 }
+
+FishList()
