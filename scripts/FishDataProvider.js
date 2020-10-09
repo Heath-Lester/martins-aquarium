30d8f19bd474626611eca4c07968af65443b82a9
Heath Lester <h.allen.lester@gmail.com>
2020-10-08 22:14:19 -0500

    Added Background Image, Margins, and flex quality fixes

--- scripts/FishDataProvider.js
+++ scripts/FishDataProvider.js
@@ -1,15 +1,15 @@
-
+// This contains all the Fish as objects with properties in the array fishCollections.
 const fishCollection = [
     {
-        image:"https://i.pinimg.com/originals/6a/66/41/6a6641a2e7275ca9764e3335d2e755c8.jpg",
+        image:"./images/Largemouth Bass.jpg",
         name: "Fred",
         species: "Largemouth Bass",
         length: 24,
         location: "Montgomery Bell State Park",
-        diet: "Bait fish, scuds, shrimp, shad, nails, crawfish, frogs, snakes, salamanders, bats, small water birds, mammals, and baby alligators."
+        diet: "smaller fish, snails, frogs, snakes, salamanders, bats, small water birds, and baby alligators."
     }, 
     {
-        image:"https://freshwaterfishingadvice.com/wp-content/uploads/2018/12/storyblocks-close-up-of-a-smallmouth-bass-with-a-lake-in-the-background_HVYE7RXWX_1_900x.jpg",
+        image:"./images/SmallmouthBass.jpg",
         name: "Frank",
         species: "Smallmouth Bass",
         length: 20,
@@ -17,7 +17,7 @@ const fishCollection = [
         diet: "Plankton, small waterbugs, other smaller smallmouth bass."
     },    
     {
-        image:"https://i.ytimg.com/vi/gnkJSWSv77I/maxresdefault.jpg",
+        image:"./images/Bluegill.jpg",
         name: "Bob",
         species: "Bluegill",
         length: 6,
@@ -25,7 +25,7 @@ const fishCollection = [
         diet: "small aquatic insects and fish"
     },    
     {
-        image:"https://bloximages.chicago2.vip.townnews.com/kokomotribune.com/content/tncms/assets/v3/editorial/4/09/40979d6e-5e2d-11e9-aafa-1f0d242e1f32/5cb24ae420704.image.jpg?resize=1200%2C900",
+        image:"./images/Crappie.jpg",
         name: "Sasha",
         species: "Crappie",
         length: 8,
@@ -34,7 +34,7 @@ const fishCollection = [
     },
 ]
 
-
+// This exports a copy of the array as a functinton. .slice is used to protect the fishCollection array from being tampered with inadvertently.
 export const useFish = () => {
     return fishCollection.slice()
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,15 @@
-// This logs all the properties of all the objects from fishCollection
+
 
 import {  useFish  } from './FishDataProvider.js';
 import {  FishList  } from './FishList.js';
 
+// This places the copy of the array fishCollection into a variable allTheFish
 const allTheFish = useFish()
 
+// This logs all the properties of all the objects from fishCollection
 for (const fish of allTheFish) {
     console.log(fish)
 }
 
+//this calls the function FishList that combines both FishDataProvider.js and Fish.js and renders them to HTML
 FishList()
